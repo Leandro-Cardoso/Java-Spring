605899065e1ac6ef81a27f8df9221e0479b8514e
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-05-08 22:47:29 -0300

    Inicializacao

---
+++ src/test/java/com/leandrocardoso/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandrocardoso.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
