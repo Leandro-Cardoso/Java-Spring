4cff8be3b592e6d306038a74cf57bcec08edb375
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-05-09 00:21:40 -0300

    Hello world

---
+++ src/main/java/com/leandrocardoso/spring/Controller.java
@@ -0,0 +1,14 @@
+package com.leandrocardoso.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/helloworld")
+    public String helloWorld() {
+        return "\nHello world !!!\n";
+    }
+}
