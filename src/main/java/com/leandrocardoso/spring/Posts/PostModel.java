3384d33ce15be552a875acb89a3459bf8e2823ae
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-05-10 19:59:02 -0300

    Introduzindo o Lombok

--- src/main/java/com/leandrocardoso/spring/Posts/PostModel.java
+++ src/main/java/com/leandrocardoso/spring/Posts/PostModel.java
@@ -1,44 +1,32 @@
 package com.leandrocardoso.spring.Posts;
 
 import com.leandrocardoso.spring.Users.UserModel;
+
+// JPA:
 import jakarta.persistence.*;
+
+// Lombok:
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "author_id") // Foreing Key
     private UserModel author;
+
     private String title;
+
     private String text;
-
-    public PostModel(String title, String text) {
-        this.title = title;
-        this.text = text;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
-
-    public String getText() {
-        return this.text;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
 
 }
--- src/main/java/com/leandrocardoso/spring/Users/UserModel.java
+++ src/main/java/com/leandrocardoso/spring/Users/UserModel.java
@@ -1,59 +1,35 @@
 package com.leandrocardoso.spring.Users;
 
 import com.leandrocardoso.spring.Posts.PostModel;
+
+// JPA:
 import jakarta.persistence.*;
+
+// Lombok:
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
+
     private String bio;
+
     @OneToMany(mappedBy = "author")
     private List<PostModel> posts;
-
-    public UserModel(String name, String email, String bio) {
-        this.name = name;
-        this.email = email;
-        this.bio = bio;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public String getBio() {
-        return this.bio;
-    }
-
-    public  List<PostModel> getPosts() {
-        return this.posts;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
 
 }
